534d93f4db5087c368f91dfc98cbd5a41f522a8f
rando <alex.nighthammer@yandex.ru>
2023-10-01 15:23:32 +0800

    z-index applied to all links now

--- src/assets/navigation/Style-01.jsx
+++ src/assets/navigation/Style-01.jsx
@@ -6,14 +6,14 @@ export default function Style01() {
 
         <div className="flex w-[90rem] h-[6.25rem] justify-between items-center px-20 py-[1.38rem]">
             <Logo />
-            <ul id="style-1" className="flex items-start gap-1 ">
-                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
-                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
-                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
-                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
-                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
+            <ul id="style-1" className="flex items-start gap-1">
+                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem] z-[100]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
+                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem] z-[100]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
+                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem] z-[100]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
+                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem] z-[100]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
+                <ol className="flex px-5 py-4 justify-center items-center gap-2 rounded-2xl"><a href="/" className="dark:text-white text-[#262626] font-inter text-base not-italic font-semibold tracking-[-.02rem] z-[100]"><span>L</span><span>i</span><span>n</span><span>k</span></a></ol>
             </ul>
-            <ButtonPrimary width="w-[8.875rem]" />
+            <ButtonPrimary width="w-[8.875rem]" zIndex="z-[100]"/>
         </div>
 
     )
