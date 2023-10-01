8a27be6c7e9ab8d097a68791b3e8549a8c70969e
rando <alex.nighthammer@yandex.ru>
2023-10-01 15:24:51 +0800

    the random function's import and name changes

--- src/assets/pages/Variant01.jsx
+++ src/assets/pages/Variant01.jsx
@@ -11,14 +11,14 @@ import PricingTable from "../pricing-tables/PricingTables.jsx"
 import Contact from "../contacts/Contact.jsx"
 import Footer from "../footers/Footer.jsx"
 import { useState } from "react"
-import { getRandomNumber } from "../../utils/getRandomIntNumber.js"
+import { getRandomIntNumber } from "../../utils/getRandom.js"
 
 const randomize = () => {
         
-    //const getRandomNumber = (max, min = 1) => Math.trunc(((Math.random() * (max - min + 1)) + 1))
-    const arrayOfRandNumbers = [getRandomNumber(11), getRandomNumber(10), getRandomNumber(5), getRandomNumber(3),
-                                getRandomNumber(3), getRandomNumber(6), getRandomNumber(5), getRandomNumber(3),
-                                getRandomNumber(4), getRandomNumber(4), getRandomNumber(4), getRandomNumber(17)]
+    //const getRandomIntNumber = (max, min = 1) => Math.trunc(((Math.random() * (max - min + 1)) + 1))
+    const arrayOfRandNumbers = [getRandomIntNumber(11), getRandomIntNumber(10), getRandomIntNumber(5), getRandomIntNumber(3),
+                                getRandomIntNumber(3), getRandomIntNumber(6), getRandomIntNumber(5), getRandomIntNumber(3),
+                                getRandomIntNumber(4), getRandomIntNumber(4), getRandomIntNumber(4), getRandomIntNumber(17)]
     return arrayOfRandNumbers
 
 }
@@ -37,7 +37,7 @@ export default function Variant01() {
     return(
         <div className="flex items-center flex-col ">
             <button className="fixed bottom-4 right-4 w-24 h-10 dark:text-[#F2EDEB] text-black font-inter text-base not-italic font-semibold outline-4 out-[#F2EDEB] outline hover:opacity-75 animate-bounce z-[9999]" onClick={handleClicking}>randomize</button>
-            <Hero style={randomSet[1]} /> 
+            <Hero style={10} /> 
             <Content style={randomSet[5]} />
             <Showcase style={randomSet[3]} />
             <Feature style={randomSet[6]} />
