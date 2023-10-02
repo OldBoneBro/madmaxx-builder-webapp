b707538643bf6e01d07dc6ba38294afc106d9686
rando <alex.nighthammer@yandex.ru>
2023-10-02 15:57:14 +0800

    switch to the Style-08 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Variant01 from "./assets/pages/Variant01.jsx"
+import Style08 from "./assets/heroes/Style-08.jsx"
 
 function App() {
 
   return (
-      <Variant01/>
+      <Style08/>
   )
 
 }
