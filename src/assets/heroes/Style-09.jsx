751929912bda750fa2db68596b9eb05d48a59375
rando <alex.nighthammer@yandex.ru>
2023-10-26 21:17:43 +0800

    a little attempt to create swapping animation for the video rectangles

--- src/assets/heroes/Style-09.jsx
+++ src/assets/heroes/Style-09.jsx
@@ -2,7 +2,7 @@ import Navigatinon from "../navigation/Navigation.jsx"
 import { PlaySvg }  from "../imgs/svgs/components/Play.jsx"
 
 export default function Style09() {
-
+    //W: 74.969 H: 74.94
     return(
 
         <div className="flex flex-col w-[90rem] items-start">
@@ -13,13 +13,13 @@ export default function Style09() {
                     <p className="w-[33.75rem] text-[#757575] text-center font-inter text-lg not-italic font-medium tracking-[-0.0225rem]">The night is dark and full of terrors. What is dead may never die. And now his watch is ended. All men must die.</p>
                 </div>
                 <div className=" relative flex w-[57.6875rem] h-[25.6875rem] items-center">
-                    <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F]"></div>
-                    <div className="absolute left-24 flex w-[45.6875rem] h-[25.6875rem] justify-center items-center rounded-[2.5rem] dark:bg-white bg-[#404040]">
-                        <div className="flex justify-center items-center w-[6.5rem] h-[6.5rem] rounded-full bg-gradient-135  from-[#00EA87] to-[#78CBFF] hover:from-[#4510ca] hover:to-[#4510ca]">
-                            <PlaySvg width="44.373" height="44.373" />
+                    <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] hover:bg-white hover:scale-x-[74.969] "></div>
+                    <div className="absolute left-24 flex w-[45.6875rem] h-[25.6875rem] justify-center items-center rounded-[2.5rem] bg-white">
+                        <div className="flex justify-center items-center w-[6.5rem] h-[6.5rem] rounded-full bg-size-200 transition-all bg-pos-0 hover:bg-pos-100 bg-gradient-135 from-[#00EA87] via-[#78CBFF] to-[#4510ca]">
+                            <PlaySvg width="44.373" height="44.373" defaultColorScheme={false}  />
                         </div>
                     </div>
-                    <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F]"></div>
+                    {/* <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F]"></div> */}
                 </div>
             </div>
         </div>
