6ceae9cd4c4475814c932afc5dd5f0d267bbdba2
rando <alex.nighthammer@yandex.ru>
2023-10-27 21:23:51 +0800

    implementing clicking logic

--- src/assets/heroes/Style-09.jsx
+++ src/assets/heroes/Style-09.jsx
@@ -1,7 +1,30 @@
 import Navigatinon from "../navigation/Navigation.jsx"
 import { PlaySvg }  from "../imgs/svgs/components/Play.jsx"
+import { useState } from "react"
+
+const objEmpty = (obj) => {
+    for (const prop in obj) {
+        if (Object.hasOwn(obj, prop)) return false
+    }
+
+    return true
+}
 
 export default function Style09() {
+
+    const [vidStyle, setVidStyle] = useState({})
+
+    const handleClick  = (event) => {
+        setVidStyle({
+            bgColor: '#FF0000',
+            xScale: '1.59',
+            yScale: '1.345',
+            zIndex: '10',
+            opacity: '0.5',
+            translate: '230.4px',
+            borderRadius: '32px'
+        })
+    }
     //W: 74.969 H: 74.94
     return(
 
@@ -13,7 +36,7 @@ export default function Style09() {
                     <p className="w-[33.75rem] text-[#757575] text-center font-inter text-lg not-italic font-medium tracking-[-0.0225rem]">The night is dark and full of terrors. What is dead may never die. And now his watch is ended. All men must die.</p>
                 </div>
                 <div className=" relative flex w-[57.6875rem] h-[25.6875rem] items-center">
-                    <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] hover:bg-[#FF0000] hover:scale-x-[1.59] hover:scale-y-[1.345] hover:z-10 hover:opacity-50 hover:translate-x-[14.4rem] hover:rounded-[2rem] transition-all hover:transition-all"></div>
+                    <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] transition-all" onClick={handleClick} ></div>
                     <div className="absolute left-24 flex w-[45.6875rem] h-[25.6875rem] justify-center items-center rounded-[2.5rem] bg-white">
                         <div className="flex justify-center items-center w-[6.5rem] h-[6.5rem] rounded-full bg-size-200 transition-all bg-pos-0 hover:bg-pos-100 bg-gradient-135 from-[#00EA87] via-[#78CBFF] to-[#4510ca]">
                             <PlaySvg width="44.373" height="44.373" defaultColorScheme={false}  />
