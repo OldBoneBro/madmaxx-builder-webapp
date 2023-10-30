a30522071d2055d8e3739e42ab908270287bb60a
rando <alex.nighthammer@yandex.ru>
2023-10-30 20:40:28 +0800

    almost done with the animation

--- src/assets/heroes/Style-09.jsx
+++ src/assets/heroes/Style-09.jsx
@@ -17,18 +17,22 @@ export default function Style09() {
     const [vidLeft, setVidLeft] = useState({})
     const [vidMid, setVidMid] = useState({})
     const [vidRight, setVidRight] = useState({})
-    const clickedElems = useRef(null)
+    const left = useRef(null)
+    const mid = useRef(null)
+    const right = useRef(null)
+    const clickedElem = useRef(null)
 
     const handleClick  = (event) => {
 
-        console.log(event.target.offsetLeft)
-        if (clickedElems.current === event.target) return
-        const styles  = [...getVidStyles(event.target.offsetLeft)]
+        if (clickedElem.current === event.target) return
+
+        let currentVidBox = (left.current === event.target) ? 1 : (right.current === event.target) ? 3 : 2
+        const styles  = [...getVidStyles(currentVidBox)]
         console.log(styles)
         setVidLeft(styles[0])
         setVidMid(styles[1])
         setVidRight(styles[2])
-        clickedElems.current = event.target
+        clickedElem.current = event.target
 
     }
 
@@ -42,32 +46,26 @@ export default function Style09() {
                     <h2 className="self-stretch dark:text-white text-[#404040] text-center font-inter text-[6rem]/[7rem] not-italic font-semibold tracking-[-0.24rem]">Build your landings in minutes</h2>
                     <p className="w-[33.75rem] text-[#757575] text-center font-inter text-lg not-italic font-medium tracking-[-0.0225rem]">The night is dark and full of terrors. What is dead may never die. And now his watch is ended. All men must die.</p>
                 </div>
-                <div className=" relative flex w-[57.6875rem] h-[25.6875rem] items-center">
+                <div className="relative flex w-[57.6875rem] h-[25.6875rem] items-center justify-center">
+                    <div className="absolute z-[9999] flex justify-center items-center w-[6.5rem] h-[6.5rem] rounded-full bg-size-200 transition-all bg-pos-0 hover:bg-pos-100 bg-gradient-135 from-[#00EA87] via-[#78CBFF] to-[#4510ca]">
+                        <PlaySvg width="44.373" height="44.373" defaultColorScheme={false}  />
+                    </div>
                     {
                         (objEmpty(vidLeft)) ? 
-                            <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] transition-all" onClick={handleClick} ></div> : 
-                            <div className="rounded-[2.5rem] bg-[#1F1F1F] transition-all" onClick={handleClick} style={vidLeft}></div>
+                            <div className="flex justify-center items-center w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] transition-all ease-in-out" onClick={handleClick} ref={left}></div> : 
+                            <div className="rounded-[2.5rem] bg-[#1F1F1F] transition-all ease-in-out" onClick={handleClick} style={vidLeft} ref={left}></div>
                     }
-                    {/* absolute left-24 */}
                     {
                         (objEmpty(vidMid)) ?
-                            <div className="absolute left-24 flex w-[45.6875rem] h-[25.6875rem] justify-center items-center rounded-[2.5rem] bg-white transition-all" onClick={handleClick}>
-                                <div className="flex justify-center items-center w-[6.5rem] h-[6.5rem] rounded-full bg-size-200 transition-all bg-pos-0 hover:bg-pos-100 bg-gradient-135 from-[#00EA87] via-[#78CBFF] to-[#4510ca]">
-                                    <PlaySvg width="44.373" height="44.373" defaultColorScheme={false}  />
-                                </div>
+                            <div className="absolute left-24 flex w-[45.6875rem] h-[25.6875rem] justify-center items-center rounded-[2.5rem] bg-white transition-all ease-in-out" onClick={handleClick} ref={mid}>
                             </div> :
-                            <div className="justify-center items-center rounded-[2.5rem] bg-white transition-all" onClick={handleClick} style={vidMid}>
-                                {
-                                    <div className="flex justify-center items-center w-[6.5rem] h-[6.5rem] rounded-full bg-size-200 transition-all bg-pos-0 hover:bg-pos-100 bg-gradient-135 from-[#00EA87] via-[#78CBFF] to-[#4510ca]">
-                                        <PlaySvg width="44.373" height="44.373" defaultColorScheme={false}  />
-                                    </div>
-                                }
+                            <div className="justify-center items-center rounded-[2.5rem] bg-white transition-all ease-in-out" onClick={handleClick} style={vidMid} ref={mid}>
                             </div> 
                     }
                     {
                         (objEmpty(vidRight)) ?
-                            <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] transition-all" onClick={handleClick}></div> :
-                            <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] transition-all" onClick={handleClick} style={vidRight}></div>
+                            <div className="w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] transition-all ease-in-out" onClick={handleClick} ref={right}></div> :
+                            <div className="flex justify-center items-center w-[34.25rem] h-[19.25rem] rounded-[2.5rem] bg-[#1F1F1F] transition-all ease-in-out" onClick={handleClick} style={vidRight} ref={right}></div>
 
                     }
                 </div>
