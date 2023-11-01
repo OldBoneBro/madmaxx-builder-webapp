d89d15539b6703b227c657488ecb0676e087a090
rando <alex.nighthammer@yandex.ru>
2023-11-01 21:05:53 +0800

    plot twist alert! this is switch to the inline styling for toggling animation instead of the GSAP one

--- src/assets/components/buttons/ThemeToggler.jsx
+++ src/assets/components/buttons/ThemeToggler.jsx
@@ -1,59 +1,46 @@
-import { gsap } from "gsap"
-import { useLayoutEffect, useRef, useState } from "react"
+import {  useState } from "react"
 
 export default function ThemeToggler() {
 
-    const toggler = useRef(null)
-    const pill = useRef(null)
-    const animation = useRef(null)
-    const [theme, setTheme] = useState('light')
+    const [darkTheme, setDarkTheme] = useState(false)
+    const [togglerStyle, setTogglerStyle] = useState({})
 
-    useLayoutEffect(() => {
-
-        let ctx = gsap.context(() => {
-            animation.current.progress(0).kill()
-            animation.current = gsap.timeline({ paused: true })
-                .to(toggler.current, {
-                    duration: 0.5,
-                    backgroundColor: "#000",
-                    ease: "power4.inOut"
-                }, 0)
-                .to(pill.current, {
-                    duration: 0.5,
-                    backgroundColor: "#fff",
-                    y: 0,
-                    ease: "power4.inOut"
-                }, 0)
-                .to(toggler.current, {
-                    duration: 0.25,
-                    backgroundColor: "#fff",
-                    ease: "power4.inOut"
-                }, 0.25)
-                .to(pill.current, {
-                    duration: 0.25,
-                    backgroundColor: "#000",
-                    y: 24, 
-                    ease: "power4.inOut"
-                }, 0.25)
-                .reverse()
-        })
-
-        return () => ctx.revert()
-
-    }, [])
 
     const handleClick = () => {
 
-        animation.current.reversed(!animation.current.reversed())
+        if (!darkTheme) {
+            setTogglerStyle({
+                shell: {
+                    backgroundColor: '#fff',
+                },
+                pill: {
+                    backgroundColor: '#000',
+                    transform: 'translateY(24px)'
+                }
+            })
+            document.documentElement.classList.toggle('dark')
+            setDarkTheme(!darkTheme)
+        } else {
+            setTogglerStyle({
+                shell: {
+                    backgroundColor: '#000',
+                },
+                pill: {
+                    backgroundColor: '#fff',
+                    transform: 'translateY(0px)'
+                }
+            })
+            document.documentElement.classList.toggle('dark')
+            setDarkTheme(!darkTheme)
+        }
+
     }
 
     return (
-        <div className="flex w-full h-screen justify-center items-center">
-            <div className="flex justify-center">
-                <button className="flex w-8 h-16 outline outline-2 outline-black bg-black justify-center p-1 rounded-lg" onClick={handleClick} ref={toggler}>
-                    <div className="w-8 h-8 bg-white rounded-lg transition-all" ref={pill}></div>
-                </button>
-            </div>
+        <div className="fixed left-8 top-[90%] z-[9999]">
+            <button className="flex w-8 h-16 outline outline-2 outline-black bg-black justify-center p-1 rounded-lg transition-all " onClick={handleClick} style={togglerStyle.shell}>
+                <div className="w-8 h-8 bg-white rounded-lg transition-all" style={togglerStyle.pill}></div>
+            </button>
         </div>
     )
 }
