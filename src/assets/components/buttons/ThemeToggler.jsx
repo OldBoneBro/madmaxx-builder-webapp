92c84172b8f3f80712a31d336273c0e8c6d601f0
rando <alex.nighthammer@yandex.ru>
2023-11-02 21:40:34 +0800

    new feature. light\dark theme now is stored in and loaded from LocalStorage of a browser

--- src/assets/components/buttons/ThemeToggler.jsx
+++ src/assets/components/buttons/ThemeToggler.jsx
@@ -1,15 +1,44 @@
-import {  useState } from "react"
+import {  useState } from 'react'
 
 export default function ThemeToggler() {
 
-    const [darkTheme, setDarkTheme] = useState(false)
-    const [togglerStyle, setTogglerStyle] = useState({})
+    const [theme, setTheme] = useState(() => {
 
+        if(!JSON.parse(localStorage.getItem('theme')).type) {
+   
+            let storageTheme = { 
+                type: 'light',
+                style: {
+                    shell: {
+                        backgroundColor: '#000',
+                    },
+                    pill: {
+                        backgroundColor: '#fff',
+                        transform: 'translateY(0px)'
+                    }
+                }
+            }
+        
+            localStorage.setItem('theme', JSON.stringify(storageTheme, 2))
+            return storageTheme
+
+        }
+
+        (JSON.parse(localStorage.getItem('theme')).type === "dark") ?
+            document.documentElement.classList.add('dark') :
+            document.documentElement.classList.remove('dark')
+
+        return JSON.parse(localStorage.getItem('theme'))
+
+    })
 
     const handleClick = () => {
 
-        if (!darkTheme) {
-            setTogglerStyle({
+        const storageTheme = JSON.parse(localStorage.getItem('theme'))
+
+        if (storageTheme.type === 'light') {
+            storageTheme.type = 'dark'
+            storageTheme.style = {
                 shell: {
                     backgroundColor: '#fff',
                 },
@@ -17,11 +46,11 @@ export default function ThemeToggler() {
                     backgroundColor: '#000',
                     transform: 'translateY(24px)'
                 }
-            })
-            document.documentElement.classList.toggle('dark')
-            setDarkTheme(!darkTheme)
+            }
+            document.documentElement.classList.add('dark')
         } else {
-            setTogglerStyle({
+            storageTheme.type = 'light'
+            storageTheme.style = {
                 shell: {
                     backgroundColor: '#000',
                 },
@@ -29,17 +58,19 @@ export default function ThemeToggler() {
                     backgroundColor: '#fff',
                     transform: 'translateY(0px)'
                 }
-            })
-            document.documentElement.classList.toggle('dark')
-            setDarkTheme(!darkTheme)
+            }
+            document.documentElement.classList.remove('dark')
         }
+
+        setTheme(storageTheme)
+        localStorage.setItem('theme', JSON.stringify(storageTheme, 2))
 
     }
 
     return (
-        <div className="fixed left-8 top-[90%] z-[9999]">
-            <button className="flex w-8 h-16 outline outline-2 outline-black bg-black justify-center p-1 rounded-lg transition-all " onClick={handleClick} style={togglerStyle.shell}>
-                <div className="w-8 h-8 bg-white rounded-lg transition-all" style={togglerStyle.pill}></div>
+        <div className='fixed left-8 top-[90%] z-[9999]'>
+            <button className='flex w-8 h-16 outline outline-2 outline-black bg-black justify-center p-1 rounded-lg transition-all ' onClick={handleClick} style={theme.style.shell}>
+                <div className='w-8 h-8 bg-white rounded-lg transition-all' style={theme.style.pill}></div>
             </button>
         </div>
     )
