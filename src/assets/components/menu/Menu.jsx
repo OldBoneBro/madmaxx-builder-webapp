c9227b09cf9785c56ceaed1a8b227e1c5ba41242
rando <alex.nighthammer@yandex.ru>
2023-11-03 19:08:34 +0800

    re-written with usage of SVG component instead of link

--- src/assets/components/menu/Menu.jsx
+++ src/assets/components/menu/Menu.jsx
@@ -1,10 +1,10 @@
-import menu from "../../imgs/svgs/icons/Menu.svg";
+import MenuIcon from "../../imgs/svgs/icons/Menu.svg?react";
 
 export default function Menu () {
 
     return (
         <div className="flex w-14 h-14 p-[0.875rem] justify-center items-center dark:bg-[#1F1F1F] bg-[#404040] rounded-2xl">
-            <img src={menu} alt="Menu" />
+            <MenuIcon />
         </div>
     )
 
