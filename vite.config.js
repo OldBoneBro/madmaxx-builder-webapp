560b1be92ec547d6e5ad9349ed3ff0dc0302c9a7
rando <alex.nighthammer@yandex.ru>
2023-11-03 19:06:01 +0800

    the @svgr/rollup didn't work, so I tried vite-plugin-svgr and it does the job

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import url from "@svgr/rollup";
+import svgr from "vite-plugin-svgr";
 
 //
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), url()],
+  plugins: [react(), svgr()],
   base: "/madmaxx-builder-webapp/",
 });
