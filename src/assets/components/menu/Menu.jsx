b0e54fc5d60f16d112be81e76b4ed469c51fd882
rando <alex.nighthammer@yandex.ru>
2023-11-04 10:18:04 +0800

    add getSvgUrl function

--- src/assets/components/menu/Menu.jsx
+++ src/assets/components/menu/Menu.jsx
@@ -1,11 +1,11 @@
-import MenuIcon from "../../imgs/svgs/icons/Menu.svg";
+import { getSvgUrl } from "../../../utils/getSvgUrl";
 
 export default function Menu () {
     
     return (
         <div className="flex w-14 h-14 p-[0.875rem] justify-center items-center dark:bg-[#1F1F1F] bg-[#404040] rounded-2xl">
             {/* <MenuIcon /> */}
-            <img src={MenuIcon} alt="" />
+            <img src={getSvgUrl("../../imgs/svgs/icons/Menu.svg")} alt="" />
         </div>
     )
 
