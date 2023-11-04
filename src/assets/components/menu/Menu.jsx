cbd8bd1bc5d4992af430b4602d2fb91cce7be049
rando <alex.nighthammer@yandex.ru>
2023-11-04 10:37:58 +0800

    remove unnecessary part

--- src/assets/components/menu/Menu.jsx
+++ src/assets/components/menu/Menu.jsx
@@ -4,7 +4,6 @@ export default function Menu () {
     
     return (
         <div className="flex w-14 h-14 p-[0.875rem] justify-center items-center dark:bg-[#1F1F1F] bg-[#404040] rounded-2xl">
-            {/* <MenuIcon /> */}
             <img src={getSvgUrl("../../imgs/svgs/icons/Menu.svg")} alt="" />
         </div>
     )
