3a5f119b1f0dddb2400da09d5e0bcfe424a9a9b2
rando <alex.nighthammer@yandex.ru>
2023-11-04 09:41:44 +0800

    i dunno, maybe this will help

--- src/assets/components/menu/Menu.jsx
+++ src/assets/components/menu/Menu.jsx
@@ -1,10 +1,11 @@
-import MenuIcon from "https://github.com/OldBoneBro/madmaxx-builder-webapp/blob/main/src/assets/imgs/svgs/Icons/Menu.svg?react";
+import MenuIcon from "../../imgs/svgs/icons/Menu.svg";
 
 export default function Menu () {
-
+    
     return (
         <div className="flex w-14 h-14 p-[0.875rem] justify-center items-center dark:bg-[#1F1F1F] bg-[#404040] rounded-2xl">
-            <MenuIcon />
+            {/* <MenuIcon /> */}
+            <img src={MenuIcon} alt="" />
         </div>
     )
 
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 //
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/Menu.svg"],
   plugins: [react(), svgr()],
   base: "/madmaxx-builder-webapp/",
 });
