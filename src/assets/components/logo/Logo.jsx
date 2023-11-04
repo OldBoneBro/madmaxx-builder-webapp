83f879c4d4b1108583d11a33b86be21c8a23f8f4
rando <alex.nighthammer@yandex.ru>
2023-11-04 10:45:44 +0800

    static imports work, getSvgUrl function doesn't

--- src/assets/components/logo/Logo.jsx
+++ src/assets/components/logo/Logo.jsx
@@ -1,13 +1,12 @@
 import Mark from "./Mark.jsx"
-import { getSvgUrl } from "../../../utils/getSvgUrl.js"
-
+import zero from "../../imgs/svgs/logo/zero.svg"
 
 export default function Logo({ origialColorScheme = true }) {
 
     return(
         <div className="flex items-center gap-2">
             <Mark origialColorScheme={origialColorScheme} />
-            <img className={`w-[5.31344rem] h-[1.59rem] select-none ${origialColorScheme && 'invert dark:invert-0'}`} src={getSvgUrl("../../imgs/svgs/logo/zero.svg")} alt="Logo" />
+            <img className={`w-[5.31344rem] h-[1.59rem] select-none ${origialColorScheme && 'invert dark:invert-0'}`} src={zero} alt="Logo" />
         </div>
     )
 
